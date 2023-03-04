7528844916b5ebe6adc240880ab70fb5fce1b5f1
griush <gerardriush@gmail.com>
2023-03-04 20:07:07 +0100

    Extended quaternions

---
+++ include/gem_base.hpp
@@ -0,0 +1,19 @@
+/*
+    made by griush
+*/
+
+#ifndef GEM_BASE_HPP
+#define GEM_BASE_HPP
+
+namespace gem {
+
+    typedef unsigned char       uint8;
+
+    typedef int                 int32;
+    typedef unsigned int        uint32;
+
+    typedef unsigned long long  uint64;
+
+}
+
+#endif
